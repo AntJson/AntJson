3a82ab0ee74abf40f5724536078c137afa8ee54f
danysmall <dany.small.work@gmail.com>
2023-05-18 16:55:31 +0300

    Added flag macro ANT_MULTI_FILE_CONSTRUCTORS

---
+++ test/CppLang/dtos/nvr/nvr.h
@@ -0,0 +1,25 @@
+#ifndef ANTJSON_NVR_H
+#define ANTJSON_NVR_H
+
+#include <string>
+#include <vector>
+#include "../account/account.h"
+#include "../../../../antjson.h"
+
+struct NVR {
+    std::string ip{};
+    std::string name{};
+    int port{};
+    Account account{};
+    std::vector<int> numbersArray;
+    std::vector<Account> accountArray;
+
+    NVR() = default;
+    explicit NVR(const std::string& json);
+    static int isEqualScheme(const std::string& json);
+
+    static int fromJson(Ant::JsonNode* source, NVR* dest);
+    static inline Ant::JsonNode* toJsonScheme();
+};
+
+#endif //ANTJSON_NVR_H
